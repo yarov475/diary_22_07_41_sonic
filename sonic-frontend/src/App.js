5d0d3233f3f70c196fb85233acd54d6d1fb93a75
yarov475 <ya.yarochkin@yandex.ru>
2021-07-26 14:24:13 +0100

    react

---
+++ sonic-frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Articles from "./components/articles";
+ import Text_tone from "./components/TextToneCSVshow";
+
+function App() {
+    return (
+        <div className="App">
+
+            <Articles/>
+            <Text_tone/>
+        </div>
+    );
+}
+
+export default App;
